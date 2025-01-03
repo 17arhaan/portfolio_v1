63a21ae699a16f9ac6ead2fb8b4f83b449c5e35e
17Arhaan <17arhaan@gmail.com>
2025-01-04 01:27:41 +0530

    contact page fix

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const menuBtn = document.getElementById('menu-btn');
     const navLinks = document.getElementById('nav-links');
 
-    // Toggle the navigation menu when the hamburger button is clicked
+    // Toggle the 'show' class when the hamburger button is clicked
     menuBtn.addEventListener('click', () => {
         navLinks.classList.toggle('show');
     });
